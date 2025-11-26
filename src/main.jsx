c97a1e5e4b8caf028dcfbc1039f7ea8d1a3444ee
Armando <armandoenriquezpuga@gmail.com>
2025-11-25 19:01:03 -0600

    basename en React main

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Page-for-Gym">
       <App />
     </BrowserRouter>
   </StrictMode>,
